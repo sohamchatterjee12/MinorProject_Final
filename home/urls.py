ca5c1740c4889bbfceefe19794e96e26297cad97
unknown <atharva.thombre30@gmail.com>
2020-11-13 22:33:16 +0530

    login and register done

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,8 @@ from home import views
 
 urlpatterns = [
     path('',views.landing_page,name='landing'),
+    path('landing_page', views.landing_page, name='landing'),
+    path('landing_page_with_context', views.landing_page_with_context, name='landing_page_with_context'),
     path('home',views.home_page,name='home'),
     path("register", views.landing_page_register, name="reg")
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,HttpResponse
+from django.contrib import auth
 ##########################Pyrebase####################################
 import pyrebase
 config = {
@@ -9,30 +10,113 @@ config = {
 }
 firebase = pyrebase.initialize_app(config)
 db = firebase.database() ##database reference
+authe = firebase.auth()   ##authentication reference
 #######################################################################
 # Create your views here.
+
 def home_page(request):
+    if request.method == 'POST':  ## signing in user
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        try:
+            user = authe.sign_in_with_email_and_password(email, password)
+            userInfo = authe.get_account_info(user['idToken'])
+
+            print(userInfo["users"][0]["emailVerified"])
+
+            if userInfo["users"][0]["emailVerified"] == False : 
+                context = {
+                    "not_verified_email" : True,
+                    "bad_login" : False,
+                    "POST_request": False
+                }
+                return landing_page_with_context(request, context)
+                
+            if userInfo["users"][0]["emailVerified"] == True:
+                # print(user)
+                tempEmail = ""
+                for i in email :
+                    if i == '@':
+                        break
+                    if i == '.':
+                        tempEmail += '@'
+                        continue
+                    tempEmail += i
+                userDB = db.child("users").child(tempEmail).get()
+                # print(userD)
+                user["displayName"] = userDB.val()["fName"]
+    
+                # print(user)
+
+                context = {
+                    'user_name': user["displayName"]
+                }
+                return render(request, 'home_page.html', context)
+        except: ## Wrong Credentials
+            context = {
+                "not_verified_email" : False,
+                'bad_login' : True,
+                "POST_request" : False
+            }
+            return landing_page_with_context(request, context)
+
     return render(request,'home_page.html')
 
-def landing_page(request):
-    return render(request,'landing_page.html')
+def landing_page_with_context(request, context):
+    return render(request, 'landing_page.html', context)
 
-def landing_page_register(request):
-    if request.method == 'POST':
+def landing_page(request):
+    if request.method == 'POST':  ## registering a user
         fName = request.POST.get("fName")
         lName = request.POST.get("lName")
         email = request.POST.get("email")
         password = request.POST.get("password")
         conPass = request.POST.get("conPass")
 
-        
+        users = db.child("users").get()
+
+        user = authe.create_user_with_email_and_password(email, password)
+        print(user)
+
+        authe.send_email_verification(user['idToken'])
+
+        count = 0
+        for user in users:
+            count += 1
+        count = count + 1
+
         data = {
+            "userId": count,
             "fName": fName,
-            "lName": lName,
-            "email": email,
-            "password": password
+            "lName": lName
         }
+        tempEmail = ""  ## generating mail for firebase DB
+        for i in email :
+            if i == '@':
+                break
+            if i == '.':
+                tempEmail += '@'
+                continue
+            tempEmail += i
 
-        db.child("users").push(data)
+        # print(tempEmail)
 
+        db.child("users").child(tempEmail).set(data)
+        
+        context = {
+            "not_verified_email" : False,
+            'bad_login' : False,
+            "POST_request" : True
+        }
+        return render(request, 'landing_page.html', context)
+    
+    context = {
+            "not_verified_email" : False,
+            'bad_login' : False,
+            "POST_request" : False
+        }
+    return render(request,'landing_page.html', context)
+
+def landing_page_register(request):
     return render(request, "landing_page_register.html")
