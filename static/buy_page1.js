6ebab638bbd1d7e52ba90f9a47ce0c3579bf358f
sohamchatterjee12 <chatterjee.soham00@gmail.com>
2020-12-06 01:49:46 +0530

    more done

--- static/buy_page1.js
+++ static/buy_page1.js
@@ -1,3 +1,16 @@
 function search_clicked() {
-    
+    var search_item=document.getElementById("search_bar").value
+    console.log(search_item)
+    console.log(typeof search_item)
+    var list_of_items=firebase.database().ref("/titles").orderByKey().equalTo(search_item);
+    list_of_items.on('value', (snapshot) =>{
+        const data = snapshot.val();
+        var keys = Object.keys(data[search_item]);
+        for(var i = 0; i < keys.length; i++){
+            var k = keys[i];
+            
+        
+        }
+
+    });
 }
